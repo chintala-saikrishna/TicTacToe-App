4b3ae050212648d15bfdce3b2a51f34c82bbf1d1
chintala-saikrishna <chintalasaikrishna2007@gmail.com>
2026-04-21 18:02:09 +0530

    UC4: Place user move on board

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToe {
@@ -9,17 +10,67 @@ public class TicTacToe {
     static char computerSymbol;
 
     public static void main(String[] args) {
+
         initializeBoard();
         tossAndAssignSymbols();
         displayTossResult();
         printBoard();
 
-       
         int slot = getUserSlot();
-        System.out.println("You entered: " + slot);
+
+        placeMove(slot, humanSymbol);
+        printBoard();
     }
 
+    static void initializeBoard() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                board[row][col] = '-';
+            }
+        }
+    }
 
+    static void printBoard() {
+        System.out.println("-------------");
+
+        for (int row = 0; row < 3; row++) {
+            System.out.print("| ");
+            for (int col = 0; col < 3; col++) {
+                System.out.print(board[row][col] + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------");
+        }
+    }
+
+    static void tossAndAssignSymbols() {
+        Random rand = new Random();
+        int toss = rand.nextInt(2);
+
+        if (toss == 0) {
+            isHumanTurn = true;
+            humanSymbol = 'X';
+            computerSymbol = 'O';
+        } else {
+            isHumanTurn = false;
+            humanSymbol = 'O';
+            computerSymbol = 'X';
+        }
+    }
+
+    static void displayTossResult() {
+        if (isHumanTurn) {
+            System.out.println("You won the toss!");
+            System.out.println("You are: " + humanSymbol);
+            System.out.println("Computer is: " + computerSymbol);
+            System.out.println("You play first.");
+        } else {
+            System.out.println("Computer won the toss!");
+            System.out.println("Computer is: " + computerSymbol);
+            System.out.println("You are: " + humanSymbol);
+            System.out.println("Computer plays first.");
+        }
+    }
 
     static int getUserSlot() {
         Scanner sc = new Scanner(System.in);
@@ -28,5 +79,12 @@ public class TicTacToe {
         int slot = sc.nextInt();
 
         return slot;
+    }
+
+    static void placeMove(int slot, char symbol) {
+        int row = (slot - 1) / 3;
+        int col = (slot - 1) % 3;
+
+        board[row][col] = symbol;
     }
 }
